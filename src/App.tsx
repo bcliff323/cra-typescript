882892301c44884ac3c13c8f6b64612647148fe0
Brian Clifford <bcliff323@gmail.com>
2019-01-24 12:22:43 -0500

    Bump react-redux-router version

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,17 @@ import { List } from './components/PoolList';
 
 // Any additional component props go here.
 interface OwnProps {
-    store: any;
     history: any;
     children?: ReactNode;
 }
 
-const Routes: React.SFC = () => (
-    <Switch>
-        <Route exact path="/" component={() => <div>Home Page</div>} />
-        <Route component={() => <div>Not Found</div>} />
-    </Switch>
+const router = (history: any) => (
+    <ConnectedRouter history={history}>
+        <Switch>
+            <Route exact path="/" component={() => <div>Home Page</div>} />
+            <Route component={() => <div>Not Found</div>} />
+        </Switch>
+    </ConnectedRouter>
 );
 
-const App: React.StatelessComponent<OwnProps> = props => {
-    debugger;
-    return (
-        <Provider store={props.store}>
-            <ConnectedRouter history={props.history}>
-                <Routes />
-            </ConnectedRouter>
-        </Provider>
-    );
-};
-
-export default App;
+export const App = ({ history }: OwnProps) => router(history);
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App } from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'react-app-polyfill/ie11';
 import createHistory from 'history/createBrowserHistory';
 import configureStore from './configureStore';
+import { Provider } from 'react-redux';
 
 const history = createHistory();
 const store = configureStore(history);
-console.log(store.getState().toJS());
 
-ReactDOM.render(
-    <App store={store} history={history} />,
-    document.getElementById('root')
+const app = (
+    <Provider store={store}>
+        <App history={history} />
+    </Provider>
 );
+
+ReactDOM.render(app, document.getElementById('root'));
 
 registerServiceWorker();
